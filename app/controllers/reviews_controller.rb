b1ea9541721cc0244dff737eb9311748b3774641
Joe Cardwell <joe.cardwell@gmail.com>
2017-04-22 03:56:40 +0000

    Allow adding of Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+	def index
+		@reviews = Review.all
+	end
+	def new
+		@review = Review.new
+		@review.user = current_user
+	end
+	def create
+		@review = Review.new(params.require(:review).permit(:stars, :summary, :body))
+		@review.user = current_user
+		@review.restaurant = Restaurant.last
+		@review.save
+
+		redirect_to Restaurant
+	end
 end
