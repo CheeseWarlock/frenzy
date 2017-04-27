e60fbb6334f3cc6a25d7a7c283da7de9124136aa
Joe Cardwell <joe.cardwell@gmail.com>
2017-04-27 03:49:00 +0000

    Add friendships to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :friendships, foreign_key: :first
+  has_many :friends, through: :friendships, source: :second
 end
